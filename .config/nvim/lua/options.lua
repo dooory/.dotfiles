1dc139294e60be2a1cb5483a834d4ee3025c75b6
dooory <78720364+dooory@users.noreply.github.com>
2026-04-26 22:48:20 +1200

    feat(nvim): set additional options

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -6,3 +6,17 @@ opt.relativenumber = true
 opt.tabstop = 4
 opt.shiftwidth = 4
 opt.expandtab = true
+
+opt.termguicolors = true
+opt.cursorline = true
+opt.scrolloff = 10
+opt.wrap = off
+
+opt.splitright = true
+opt.splitbelow = true
+
+opt.clipboard = "unnamedplus"
+
+opt.mouse = "a"
+opt.undofile = true
+opt.swapfile = false
