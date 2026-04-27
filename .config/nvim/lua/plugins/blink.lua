e8b2e41444d56e45e3cffb4a2b5588c913170be5
dooory <78720364+dooory@users.noreply.github.com>
2026-04-27 12:45:50 +1200

    feat(nvim): add plugin, friendly-snippets

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -1,6 +1,7 @@
 vim.pack.add({
     "https://github.com/saghen/blink.cmp",
     "https://github.com/saghen/blink.lib",
+    "https://github.com/rafamadriz/friendly-snippets",
 })
 
 local blink = require("blink.cmp")
