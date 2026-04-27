6af2a2f0e434581f974409226613127d61be1e39
dooory <78720364+dooory@users.noreply.github.com>
2026-04-27 15:29:50 +1200

    feat(nvim): add keymap for which key + change preset to helix

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -1,1 +1,13 @@
 vim.pack.add({ "https://github.com/folke/which-key.nvim" })
+
+local whichkey = require("which-key")
+
+whichkey.setup({
+    preset = "helix",
+})
+
+vim.keymap.set("n", "<leader>?", function()
+    whichkey.show({ global = false })
+end, {
+    desc = "Buffer Local Keymaps (which-key)",
+})
